1e55e70964e7277cdc4331c6cda1ca7fa899d4b2
sapphinebz <sapphinebz@gmail.com>
2022-11-27 07:30:17 +0700

    add comments

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // for detail
+  // https://expressjs.com/en/resources/middleware/cors.html
   app.enableCors({ origin: ['http://localhost:4200'] });
   await app.listen(3000);
 }
